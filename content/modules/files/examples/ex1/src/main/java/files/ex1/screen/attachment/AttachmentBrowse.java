2fbf5d326f548e24ef5855d45a19e76daa0e3a9a
Olga Shiryaeva <o.shiryaeva@haulmont.com>
2022-04-15 16:29:36 +0400

    Describe uploading and downloading files #367

--- content/modules/files/examples/ex1/src/main/java/files/ex1/screen/attachment/AttachmentBrowse.java
+++ content/modules/files/examples/ex1/src/main/java/files/ex1/screen/attachment/AttachmentBrowse.java
@@ -8,20 +8,15 @@ import io.jmix.core.FileStorageLocator;
 import io.jmix.ui.UiComponents;
 import io.jmix.ui.action.BaseAction;
 import io.jmix.ui.component.*;
+import io.jmix.ui.download.DownloadFormat;
 import io.jmix.ui.download.Downloader;
-import io.jmix.ui.screen.*;
 import io.jmix.ui.screen.LookupComponent;
+import io.jmix.ui.screen.*;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
@@ -74,7 +69,7 @@ public class AttachmentBrowse extends StandardLookup<Attachment> {
         saveToLocalFile(attachment, Paths.get("."));
     }
 
-    // tag::file-storage[]
+    // tag::file-storage-1[]
     @Autowired
     private FileStorageLocator fileStorageLocator; // <1>
     @Autowired
@@ -97,19 +92,57 @@ public class AttachmentBrowse extends StandardLookup<Attachment> {
             throw new RuntimeException("Error getting image", e);
         }
     }
+    // end::file-storage-1[]
 
+    // tag::file-storage-2[]
     private void saveToLocalFile(Attachment attachment, Path path) {
         FileStorage fileStorage = fileStorageLocator.getDefault();
         FileRef fileRef = attachment.getFile();
-        // <5>
+        // <1>
         InputStream inputStream = fileStorage.openStream(fileRef);
         try {
-            // <6>
+            // <2>
             Files.copy(inputStream, path.resolve(fileRef.getFileName()),
                     StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new RuntimeException("Error saving image", e);
         }
     }
-    // end::file-storage[]
+    // end::file-storage-2[]
+
+
+    private void openAttachmentInNewWindow(Attachment attachment) {
+        // tag::show-file[]
+        downloader.setShowNewWindow(true);
+        downloader.download(attachment.getFile());
+        // end::show-file[]
+    }
+
+    private void downloadAttachmentWithCustomFormat(Attachment attachment) {
+        // tag::download-format[]
+        DownloadFormat txtFormat = new DownloadFormat("txt-mime-type", "txt");
+        downloader.download(attachment.getFile(), txtFormat);
+        // end::download-format[]
+    }
+
+    private void downloadAttachmentByUrl() {
+        // tag::download-url[]
+        try {
+            URLConnection connection = new URL("https://picsum.photos/300").openConnection();
+            try (InputStream responseStream = connection.getInputStream()) {
+                byte[] fileArray = IOUtils.toByteArray(responseStream);
+                downloader.download(fileArray, "photo.jpg", DownloadFormat.JPG);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error getting image", e);
+        }
+        // end::download-url[]
+    }
+
+    private void downloadFileAsByteArray() {
+        // tag::download-array[]
+        byte[] note = "A very small text note".getBytes();
+        downloader.download(note, "note", DownloadFormat.TEXT);
+        // end::download-array[]
+    }
 }
--- content/modules/files/examples/ex1/src/main/java/files/ex1/screen/attachment/AttachmentEdit.java
+++ content/modules/files/examples/ex1/src/main/java/files/ex1/screen/attachment/AttachmentEdit.java
@@ -1,13 +1,40 @@
 package files.ex1.screen.attachment;
 
 import files.ex1.entity.Attachment;
-import io.jmix.ui.screen.EditedEntityContainer;
-import io.jmix.ui.screen.StandardEditor;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.core.FileRef;
+import io.jmix.ui.component.BrowserFrame;
+import io.jmix.ui.component.FileStorageResource;
+import io.jmix.ui.component.SingleFileUploadField;
+import io.jmix.ui.model.InstanceContainer;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Attachment.edit")
 @UiDescriptor("attachment-edit.xml")
 @EditedEntityContainer("attachmentDc")
 public class AttachmentEdit extends StandardEditor<Attachment> {
+
+    @Autowired
+    private BrowserFrame browserFrame;
+    @Autowired
+    private InstanceContainer<Attachment> attachmentDc;
+
+    @Subscribe("fileField")
+    public void onFileFieldFileUploadSucceed(SingleFileUploadField.FileUploadSucceedEvent event) {
+        FileRef fileRef = attachmentDc.getItem().getFile();
+        //tag::browser-frame-mime-type[]
+        browserFrame.setSource(FileStorageResource.class)
+                .setFileReference(fileRef)
+                .setMimeType("text/plain");
+        //end::browser-frame-mime-type[]
+    }
+
+    private void showFrameWithType() {
+        //tag::browser-frame[]
+        FileRef fileRef = attachmentDc.getItem().getFile();
+        browserFrame.setSource(FileStorageResource.class)
+                .setFileReference(fileRef);
+        //end::browser-frame[]
+    }
+
 }
